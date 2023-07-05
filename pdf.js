77079a7944d4a1a47b4fc19f8f049c3b749d6b51
Sudhanshu Yadav <sudhanshuy748@gmail.com>
2023-07-05 14:17:06 +0530

    Initial commit

---
+++ pdf.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+<script>
+document.write(6+8);
+</script>
+</head>
+<body>
+<h1> html page</h1>
+<p>my paragraph</p>
+<script>
+document.write(8+8);
+</script>
+</body>
+</html>
